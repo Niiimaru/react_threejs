f7f93e82c359526f4e087f3fb5e4fd576867da1a
ueta_taiki <taixizhitian@gmail.com>
2025-04-20 10:09:02 +0900

    コミット対象のファイルのみフォーマットされるか確認

--- src/components/three-fiber/AnimationController.tsx
+++ src/components/three-fiber/AnimationController.tsx
@@ -28,6 +28,9 @@ const AnimationController: React.FC<AnimationControllerProps> = ({ cameraRef, bo
   const test = "hello world";
   console.log(test);
 
+  const test2 = "hello world";
+  console.log(test2);
+
   // スクロール割合を 0～1 の値にスケーリングする関数
   function scalePercent(scroll: number, start: number, end: number): number {
     return (scroll - start) / (end - start);
